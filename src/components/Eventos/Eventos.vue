8c86687d3bf76d78535554bcadafd60b394dfd58
Jaczibeth <jaczicruz@gmail.com>
2025-03-27 00:32:27 -0600

    agregue un segundo boton

--- src/components/Eventos/Eventos.vue
+++ src/components/Eventos/Eventos.vue
@@ -3,6 +3,7 @@
     <div class="row mt-4">
         <div>
             <button @click="Click" class="btn btn-danger"  type="button">Dale Clic</button>
+            <button @click="soyButon" class="btn btn-warning mx-2"  type="button">Dale Clic 2</button>
         </div>
        
     </div>
@@ -13,6 +14,9 @@ export default {
     methods:{
         Click(){
             alert("Gracias por darle clic")
+        },
+        soyButon(){
+            alert("soy un boton")
         }
     }
 }

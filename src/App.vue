f3c574020e48f3db2165632139a7501327aa2e04
Jaczibeth <jaczicruz@gmail.com>
2025-03-27 00:27:04 -0600

    Creando el primer evento

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,9 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import Recorrido from './components/UsoDeVariantes y V-For/Recorrido.vue';
-import Horario from './components/Horario/Horario.vue';
-import HorarioJaczibeth from './components/Horario/HorarioJaczibeth.vue';
-import RecorridoJaczibeth from './components/UsoDeVariantes y V-For/RecorridoJaczibeth.vue';
-import Padre from './components/Props/Padre.vue';
-import Hijo1 from './components/Props/Hijo1.vue';
-import Hijo2 from './components/Props/Hijo2.vue';
-import card from './components/Card/card.vue';
-import CardPrincipal from './components/Card/CardPrincipal.vue';
-import Tulipan from './components/Tulipanes/Tulipan.vue';
-import Gatos from './components/Gatos/Gatos.vue';
-import GatosC from './components/Gatos/CardsGatos/GatosC.vue';
+import Eventos from './components/Eventos/Eventos.vue';
+
 </script>
 
 <template>
- <!-- <Tulipan/>--> 
- <!--<CardPrincipal/> --> 
- <!-- <card/>--> 
-<!-- <RecorridoJaczibeth/> -->
- <!-- <Recorrido  /> -->
- <!-- <Horario /> -->
- <!--<HelloWorld />-->
-  <!-- <HorarioJaczibeth /> -->
-  <!--<Padre/>-->
-  <!-- <Gatos/> -->
-   <!-- <GatosC/> -->
+<Eventos/>
 </template>
 <style scoped> </style>
---
+++ src/components/Eventos/Eventos.vue
@@ -0,0 +1,20 @@
+<template >
+ <div class="container">
+    <div class="row mt-4">
+        <div>
+            <button @click="Click" class="btn btn-danger"  type="button">Dale Clic</button>
+        </div>
+       
+    </div>
+     </div>
+</template>
+<script>
+export default {
+    methods:{
+        Click(){
+            alert("Gracias por darle clic")
+        }
+    }
+}
+</script>
+<style ></style>

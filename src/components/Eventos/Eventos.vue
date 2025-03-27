247c8ad0c831ea4f58f7b2f3a09804af091728df
Jaczibeth <jaczicruz@gmail.com>
2025-03-27 00:55:17 -0600

    Creando un segundo evento

--- src/components/Eventos/Eventos.vue
+++ src/components/Eventos/Eventos.vue
@@ -2,22 +2,40 @@
  <div class="container">
     <div class="row mt-4">
         <div>
+            <h4> Evento Click</h4>
             <button @click="Click" class="btn btn-danger"  type="button">Dale Clic</button>
             <button @click="soyButon" class="btn btn-warning mx-2"  type="button">Dale Clic 2</button>
         </div>
-       
+        <div>
+            <h4> Evento Change</h4>
+           <label class="input-group-text"   far="">Ingresa tu nombre </label>
+           <input  class="form-control mt-2" type="text" id="nombre" @change="nombreE">
+           <p> El nombre ingresado es: <strong>{{ nombre }}</strong></p>
+        </div>
     </div>
      </div>
 </template>
 <script>
 export default {
+    data(){
+        return{
+            nombre:''
+
+        }
+        },
     methods:{
         Click(){
             alert("Gracias por darle clic")
         },
         soyButon(){
             alert("soy un boton")
+        },
+        nombreE(event){
+            this.nombre = event.target.value
+
         }
+
+
     }
 }
 </script>

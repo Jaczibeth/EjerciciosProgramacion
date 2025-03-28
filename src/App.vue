18609e9697ec58de780beb895a0a2f4a16387231
Jaczibeth <jaczicruz@gmail.com>
2025-03-27 20:40:59 -0600

    Creacion de mi propio props

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup>
 import Eventos from './components/Eventos/Eventos.vue';
 import EventosJaczibeth from './components/Eventos/EventosJaczibeth.vue';
+import PadreJ from './components/PropsJaczibeth/PadreJ.vue';
 
 
 </script>
@@ -8,6 +9,6 @@ import EventosJaczibeth from './components/Eventos/EventosJaczibeth.vue';
 <template>
 <!-- <Eventos/> -->
  <!-- <EventosJaczibeth/> -->
-  
+  <PadreJ/>
 </template>
 <style scoped> </style>
---
+++ src/components/PropsJaczibeth/Hijok.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+ edad: String,
+})
+</script>
+
+<template>
+
+<p> Tengo la edad de:{{ edad }}</p>
+</template>
+
+<style scoped></style>

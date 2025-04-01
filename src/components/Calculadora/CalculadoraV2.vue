6aaf58876e91a547bef64fb1f0af823dee7252ef
Jaczibeth <jaczicruz@gmail.com>
2025-04-01 15:19:10 -0600

    Cmbie estilos y aplique metodos en la calculadorav2

--- src/components/Calculadora/CalculadoraV2.vue
+++ src/components/Calculadora/CalculadoraV2.vue
@@ -4,11 +4,17 @@
         return{
             display:'',
             currentValue:0, 
+           
         }
     },
     methods:{
         agregarNumero(numero) {
-            this.display += numero ;
+           
+            if(numero ==='%'){
+                this.display=(parseFloat(this.display)/100).toString();
+            } else{
+                this.display += numero ;
+            }
         },
         calculo()  {
             try{
@@ -23,7 +29,8 @@
         } ,
         eliminarNumero(){
             this.display = this.display.slice(0,-1);
-        }
+        },
+       
     }
  }
    
@@ -31,16 +38,16 @@
 
 <template>
     <div class="container d-flex justify-content-center mt-4" style=" height:350px; ">
-        <div class="card p-3" style="width:300px; background-color: #DCD3FF;">
+        <div class="card p-3" style="width:300px; background-color: #add8e6;">
         <div class="card-body">
-            <h6 class="text-center ">    Calculadora V2 </h6>   
+            <h6 class="text-center "style=" font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;" >  CALCULADORA V2 </h6>   
              <!--Display de la calculadora-->
              <div class="mb-3"> 
                 <input type="text" class="form-control" v-model="display" disabled>
              </div>
              <div class="d-flex justify-content-between">
-                    <button class="btn btn-secondary" @click="">%</button>
-                    <button class="btn btn-secondary" @click="limpiarDisplay">CE</button>   
+                    <button class="btn btn-secondary" @click="agregarNumero('%')">%</button>
+                    <button class="btn btn-secondary" @click="limpiarDisplay" style="font-size: 11px;">CE</button>   
                 <button class="btn btn-secondary" @click="limpiarDisplay">C</button>
                 <button class="btn btn-danger" @click="eliminarNumero">B</button>
                 </div>
@@ -57,7 +64,7 @@
                 <button class="btn btn-secondary" @click="agregarNumero('6')">6</button>
                 <button class="btn btn-danger" @click="agregarNumero('*')">x</button>
                 </div>
-                <div class="d-flex justify-content-between mt-2">
+                <div class="d-flex justify-content-between  mt-2">
                     <button class="btn btn-secondary" @click="agregarNumero('1')">1 </button>
                     <button class="btn btn-secondary" @click="agregarNumero('2')">2</button>
                 <button class="btn btn-secondary" @click="agregarNumero('3')">3</button>
@@ -67,10 +74,18 @@
                     <button class="btn btn-secondary" @click="calculo">= </button>
                     <button class="btn btn-secondary" @click="agregarNumero('0')">0</button>
                 <button class="btn btn-secondary" @click="agregarNumero('.')">.</button>
-                <button class="btn btn-danger" @click="agregarNumero('+')">+</button>
+                <button class="btn btn-danger calc-btn" @click="agregarNumero('+')">+</button>
                 </div>
         </div>
     </div>
 </div>
 </template> 
-<style></style>
+<style>
+.btn{
+height:35px;
+width: 38px;
+text-align:center;
+color: aliceblue;
+background-color: #a2c8cc;
+}
+</style>

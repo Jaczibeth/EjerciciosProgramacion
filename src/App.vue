aa0a1e441d3258fb01e76cc9284b5ab3a4f94355
Jaczibeth <jaczicruz@gmail.com>
2025-04-01 01:43:44 -0600

    Creamos la calculadora v2

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <script setup>
-import CalculadoraV1 from './components/Calculadora/CalculadoraV1.vue';
+import CalculadoraV2 from './components/Calculadora/CalculadoraV2.vue';
 
 
 
 </script>
 
 <template>
-<CalculadoraV1/>
+<CalculadoraV2/>
 </template>
 <style scoped> </style>
